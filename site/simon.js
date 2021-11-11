1ec04b8fcd7a1f91e91120f8c7ff6249681c1c56
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-11 14:48:50 -0600

    game is complete

--- site/simon.js
+++ site/simon.js
@@ -3,6 +3,7 @@
 const greenGameControl = document.getElementById('green-game-control');
 const redGameControl = document.getElementById('red-game-control');
 const gameOver = document.getElementById('game-over');
+const gameButtons = document.getElementsByClassName('game-button');
 
 const keys = {
     blue: {
@@ -31,8 +32,22 @@ let index = 0;
 let correct = true;
 let playing = false;
 let yourTurn = false;
+let timerIndex = 0;
 
-const gameButtons = document.getElementsByClassName('game-button');
+
+greenGameControl.addEventListener('click', () => {
+    gameOver.innerHTML = '';
+    if (!playing) {
+        round();
+    }
+    playing = true;
+
+})
+
+redGameControl.addEventListener('click', () => {
+    gameOver.innerHTML = '';
+    playing = false;
+})
 
 
 const playerListener = (event) => {
@@ -50,51 +65,41 @@ const playerListener = (event) => {
     }
 }
 
-const endGame = () => {
-    const gameOverAudio = new Audio('./sounds/wrong-buzzer.mp3');
-    gameOverAudio.play();
-    gameOver.innerHTML = `You guessed incorrectly.  You made it to round ${index + 1}`;
-    sequence = [];
-    userSequence = [];
-    index = 0;
-    correct = false;
-    playing = false;
-    yourTurn = false;
-    console.log('end');
+const playerTurn = () => {
+    for (let i = 0; i < gameButtons.length; i++) {
+        gameButtons[i].addEventListener('click', playerListener)
+    };
 }
 
 const runTimer = () => {
     yourTurn = false;
     
     const timer = setInterval(() => {
-    
-        for (let value of Object.values(keys)) {
-            value.tag.removeAttribute('style');
-        }
-        const currentKey = keys[keyNums[sequence[index]]];
-        
-        currentKey.sound.play();
-        currentKey.tag.style.animation = '1s light-up';
-        index++;
-    
-        if (index === sequence.length) {
-            clearInterval(timer);
-        }
-    }, 1000);
+        if (timerIndex % 2) {
+            removeAnimation();
+        } else {
+            if (index === sequence.length) {
+                clearInterval(timer);
+            }
+            
+            const currentKey = keys[keyNums[sequence[index]]];
+            
+            currentKey.sound.play();
+            currentKey.tag.style.animation = '1s light-up';
+            index++;
 
+        }
+        timerIndex++;
+    }, 500);
 
-    for (let value of Object.values(keys)) {
-        value.tag.removeAttribute('style');
-    }
     yourTurn = true;
 }
 
-function playerTurn () {
-    for (let i = 0; i < gameButtons.length; i++) {
-        gameButtons[i].addEventListener('click', playerListener)
-    };
+const removeAnimation = () => {
+    for (let value of Object.values(keys)) {
+        value.tag.removeAttribute('style');
+    }
 }
-
 
 const round = () => {
     correct = true;
@@ -108,26 +113,21 @@ const round = () => {
     index = 0;
     const random = Math.floor(Math.random() * 4);
     sequence.push(random);
-    console.log(sequence);
+    
     runTimer();
-    
-    for (let value of Object.values(keys)) {
-        value.tag.style = '';
-    }
-    
+    removeAnimation();    
     playerTurn();
-
+    
 }
 
-greenGameControl.addEventListener('click', () => {
-    gameOver.innerHTML = '';
-    if (!playing) {
-        round();
-    }
-    playing = true;
-
-})
-
-redGameControl.addEventListener('click', () => {
+const endGame = () => {
+    const gameOverAudio = new Audio('./sounds/wrong-buzzer.mp3');
+    gameOverAudio.play();
+    gameOver.innerHTML = `You guessed incorrectly.  You made it to round ${index + 1}`;
+    sequence = [];
+    userSequence = [];
+    index = 0;
+    correct = false;
     playing = false;
-})
+    yourTurn = false;
+}
