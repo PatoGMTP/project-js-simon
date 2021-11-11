0718327fd74b3a88a819c2f3857a7220b1d61f41
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-11 14:16:57 -0600

    Game is functioning normally

--- site/simon.js
+++ site/simon.js
@@ -1,6 +1,9 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
 const greenGameControl = document.getElementById('green-game-control');
+const redGameControl = document.getElementById('red-game-control');
+const gameOver = document.getElementById('game-over');
+
 const keys = {
     blue: {
         tag: document.getElementById('blue'),
@@ -22,31 +25,47 @@ const keys = {
 
 const keyNums = ['blue', 'green', 'red', 'yellow'];
 
-const sequence = [];
+let sequence = [];
 let userSequence = [];
 let index = 0;
 let correct = true;
+let playing = false;
+let yourTurn = false;
+
 const gameButtons = document.getElementsByClassName('game-button');
 
-// for (let i = 0; i < gameButtons.length; i++) {
-//     gameButtons[i].addEventListener('click', (event) => {
-//         keys[event.target.id].sound.play();
-//         userSequence.push(keyNums.indexOf(event.target.id));
-//         console.log(userSequence);
-//     })
-// };
 
 const playerListener = (event) => {
-    keys[event.target.id].sound.play();
-    userSequence.push(keyNums.indexOf(event.target.id));
-    console.log(userSequence);
-    userSequence.forEach((num, i) => num === sequence[i] ? '' : correct = false);
-    if (userSequence.length === sequence.length) {
-        round();
+    if (yourTurn) {
+        keys[event.target.id].sound.play();
+        event.target.style.animation = '1s light-up';
+        userSequence.push(keyNums.indexOf(event.target.id));
+        userSequence.forEach((num, i) => {
+            console.log([num, sequence[i]]);
+            num === sequence[i] ? '' : endGame()
+        });
+        if (correct && sequence.length === userSequence.length) {
+            round();
+        }
     }
 }
 
+const endGame = () => {
+    const gameOverAudio = new Audio('./sounds/wrong-buzzer.mp3');
+    gameOverAudio.play();
+    gameOver.innerHTML = `You guessed incorrectly.  You made it to round ${index + 1}`;
+    sequence = [];
+    userSequence = [];
+    index = 0;
+    correct = false;
+    playing = false;
+    yourTurn = false;
+    console.log('end');
+}
+
 const runTimer = () => {
+    yourTurn = false;
+    
     const timer = setInterval(() => {
     
         for (let value of Object.values(keys)) {
@@ -60,11 +79,14 @@ const runTimer = () => {
     
         if (index === sequence.length) {
             clearInterval(timer);
-            for (let value of Object.values(keys)) {
-                value.tag.disabled = false;
-            }  
         }
     }, 1000);
+
+
+    for (let value of Object.values(keys)) {
+        value.tag.removeAttribute('style');
+    }
+    yourTurn = true;
 }
 
 function playerTurn () {
@@ -75,24 +97,37 @@ function playerTurn () {
 
 
 const round = () => {
+    correct = true;
     userSequence = [];
     for (let i = 0; i < gameButtons.length; i++) {
         gameButtons[i].removeEventListener('click', playerListener);
     }
     for (let value of Object.values(keys)) {
-        value.tag.removeAttribute('style');
+        value.tag.style = '';
     }
     index = 0;
     const random = Math.floor(Math.random() * 4);
     sequence.push(random);
     console.log(sequence);
     runTimer();
+    
+    for (let value of Object.values(keys)) {
+        value.tag.style = '';
+    }
+    
     playerTurn();
 
 }
 
 greenGameControl.addEventListener('click', () => {
-    // const startTimer = () => round();
-    round();
-    // startTimer();
+    gameOver.innerHTML = '';
+    if (!playing) {
+        round();
+    }
+    playing = true;
+
+})
+
+redGameControl.addEventListener('click', () => {
+    playing = false;
 })
